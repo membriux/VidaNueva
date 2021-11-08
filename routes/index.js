348d463156a556c5e7d88643537f2cb2ae0c8830
Guillermo <Sanguillermo98@gmail.com>
2021-11-07 20:18:26 -0500

    Updated for SEO and reduced UI clutter

--- routes/index.js
+++ routes/index.js
@@ -5,43 +5,43 @@ let fs = require('fs');
 const youtube = require('../tools/youtube');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
-  youtubeTools.isLiveNow( function(data) {
+  youtubeTools.isLiveNow(function (data) {
     if (data.items.length > 0) {
-    
+
       return res.render('index', {
         title: 'Vida Nueva',
-        description: 'Estamos en Vivo ahora!',
+        description: 'Estamos en Vivo ahora! Unete con nosotros para escuchar la palabra de Dios con nuestro Pastor Mario Zambrano! Estamos en vivo todos los Domingos a las 3pm!',
         liveNow: true
       });
 
     } else {
-    
-      res.render('index', { 
+
+      res.render('index', {
         title: 'Vida Nueva',
-        description: 'Una iglesia enfocade en Amor, Aceptacion, y Perdon',
+        description: 'Una iglesia enfocade en Amor, Aceptacion, y Perdon. Nosotros creemos en el desarrollo de la madurez espiritual y familiar, la educación, y el desarrollo del carácter cristiano por medio de la Fe.',
       });
     }
 
   })
 });
 
-router.get('/mensajes', function(req, res, next) {
+router.get('/mensajes', function (req, res, next) {
 
-  youtubeTools.isLiveNow( function(data) {
+  youtubeTools.isLiveNow(function (data) {
     if (data.items.length > 0) {
       res.render('mensajes', {
         title: 'Mensajes',
-        description: 'En Vivo ahora!',
+        description: 'Estamos en Vivo ahora! Unete con nosotros para escuchar la palabra de Dios con nuestro Pastor Mario Zambrano! Estamos en vivo todos los Domingos a las 3pm!',
         livestream: `https://www.youtube.com/embed/${data.items[0].id.videoId}?autoplay=1`
       });
 
     } else {
-      youtubeTools.getVideosList( function(videos) {
+      youtubeTools.getVideosList(function (videos) {
         res.render('mensajes', {
-          title: 'Mensajes', 
-          description: 'Escucha nuestros mensajes recientes',
+          title: 'Mensajes',
+          description: 'Escucha nuestros mensajes recientes de la Iglesia Cuadrangular Vida Nueva en San Leandro, CA. Una iglesia enfocada en Amor, Aceptacion, y Perdon.',
           videos: videos
         });
       });
@@ -50,49 +50,49 @@ router.get('/mensajes', function(req, res, next) {
   });
 });
 
-router.get('/conectate', function(req, res, next) {
+router.get('/conectate', function (req, res, next) {
   fs.readFile('data/conectate_data.json', (err, data) => {
     if (err) throw err;
     let leader_data = JSON.parse(data);
     res.render('conectate', {
-      title: 'Conectate', 
-      description: 'Conectate con nuestra iglesia',
+      title: 'Conectate',
+      description: 'Conectate con nuestra iglesia. Nuestra familia te la bienvenida para todos los hombres, mujeres, jovenes, y niños. Tu familia te esta esperando!',
       leaders: leader_data
     });
   });
- 
+
 });
 
-router.get('/creemos', function(req, res, next) {
+router.get('/creemos', function (req, res, next) {
   res.render('creemos', {
-    title: 'Creemos', 
-    description: 'Conoce sobre lo que cree la iglesia cuadrangular.'
+    title: 'Creemos',
+    description: 'Esta es una denominación eminentemente pentecostal en su doctrina y forma de adoración. La Iglesia Cuadrangular se llama así porque la doctrina de la iglesia reconoce cuatro aspectos principales de Jesucristo.'
   });
 });
 
-router.get('/donar', function(req, res, next) {
+router.get('/donar', function (req, res, next) {
   res.render('donar', {
-    title: 'Donar', 
-    description: 'Dona a tu Iglesia Vida Nueva'
+    title: 'Donar',
+    description: 'Dona a tu Iglesia Vida Nueva.'
   });
 });
 
-router.get('/registrar', function(req, res, next) {
-  res.redirect('https://tuvidanuevasl.churchcenter.com/registrations')
-});
 
-router.get('/beca', function(req, res, next) {
+router.get('/beca', function (req, res, next) {
   res.render('beca', {
-    title: 'Vida Nueva - Beca',
-    description: 'Aplica hoy!'
+    title: 'Vida Nueva Impulso Academico Scholarship',
+    description: 'Vida Nueva Impulso Académico Scholarship. Esta beca proporcionará apoyo financiero a cada candidato que es elegido basado en su estatus educativo, membresía de la Iglesia Vida Nueva, y al compromiso al estudio o carrera.'
   })
 })
 
-router.get('/test', function(req, res, next) {
-  res.render('test', {
-    title: 'Test'
-  });
+router.get('/registrar', function (req, res, next) {
+  res.redirect('https://tuvidanuevasl.churchcenter.com/registrations')
 });
+
+router.get('/yt', function (req, res, next) {
+  res.redirect('https://www.youtube.com/c/VidaNuevaSanLeandro')
+});
+
 
 router.get('/robots.txt', function (req, res, next) {
   res.type('text/plain')

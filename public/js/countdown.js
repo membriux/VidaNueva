a685aeff63fb8f76219ef5733d5666bc48437579
Guillermo <Sanguillermo98@gmail.com>
2021-11-07 20:57:47 -0500

    fixed counter issue conditionals

--- public/js/countdown.js
+++ public/js/countdown.js
@@ -13,31 +13,25 @@ function getNextSunday() {
     let nowDay = now.getDay()
     let nowHour = now.getHours()
     let nowMinutes = now.getMinutes()
-    
-    if (nowDay == 0) {
-        // Edge Case –– Sunday before 3:00 pm, don't change day
-        if (nowHour < 15) { 
-            return now 
-        }
-        
-        // Between 3 - 5pm on sunday ask to refresh page
-        else if (15 < nowHour < 17 ) {
-            return askToRefreshPage()
-        } 
 
-    } 
-    
+    if (nowDay == 0 && nowHour > 15 && nowHour < 17) {
+        return askToRefreshPage()
+    } else if (nowDay == 0 && nowHour < 15) {
+        return now
+    }
+
     let daysUntilSunday = 7 - now.getDay()
-    // Set the time of service
-    now.setDate(now.getDate() + daysUntilSunday) 
 
+    // Set the time of service
+    now.setDate(now.getDate() + daysUntilSunday)
     // Sunday after live stream get day left
     now.setHours(15)
     now.setMinutes(30)
     now.setSeconds(00)
-    
+
     return now
 }
+
 
 function askToRefreshPage() {
     countdownText.empty()
@@ -52,21 +46,18 @@ function askToRefreshPage() {
 }
 
 
-
-function getTimeRemaining(endtime){
+function getTimeRemaining(endtime) {
     const total = Date.parse(endtime) - Date.parse(new Date());
-    const seconds = Math.floor( (total/1000) % 60 );
-    const minutes = Math.floor( (total/1000/60) % 60 );
-    const hours = Math.floor( (total/(1000*60*60)) % 24 );
-    const days = Math.floor( total/(1000*60*60*24) );
-  
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
     return { total, days, hours, minutes, seconds };
-  }
-  
+}
 
 function initializeClock(endtime) {
     const timeinterval = setInterval(() => {
-
         const t = getTimeRemaining(endtime);
         if (t.days > 0) {
             clock.html(`${t.days} días ${t.hours} hrs ${t.minutes} min ${t.seconds} sec`);
@@ -77,12 +68,7 @@ function initializeClock(endtime) {
             clock.html(`${t.minutes} min ${t.seconds} sec`);
         }
 
-        if (t.total <= 0) {
-            
-            askToRefreshPage();
-        }
-
-    },1000);
+    }, 1000);
 }
 
 

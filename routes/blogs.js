32822684155eac505393ec2b28411adc39214bf3
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2024-09-06 00:45:38 -0700

    Database integration.

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const Parse = require('parse/node');
 
-// Route to render the blog page
-router.get('/', function(req, res, next) {
-  res.render('blog', { title: 'Blog Page' });
+router.get('/', async function(req, res, next) {
+  const Blog = Parse.Object.extend('Blog');
+  const query = new Parse.Query(Blog);
+
+  // Fetch all blog entries
+  const results = await query.find();
+
+  // Extract the relevant fields for each blog post
+  const blogs = results.map(blogPost => ({
+    title: blogPost.get('Title'),
+    subtitle: blogPost.get('Subtitle'),
+    description: blogPost.get('Description'),
+    imageUrl: blogPost.get('Title_image')
+  }));
+
+  // Log the blogs array for debugging
+  console.log('Blogs:', blogs);
+
+  // Render the blog page and pass the blogs array to the view
+  res.render('blog', { title: 'Blog Page', blogs: blogs });
 });
+
 
 module.exports = router;
